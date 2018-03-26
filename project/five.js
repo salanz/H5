9b1e9049d355d653faaab39e3c289c3362ad2a91
alan <salanz@163.com>
2018-03-26 16:15:14 +0800

    five.js

--- project/five.js
+++ project/five.js
@@ -156,6 +156,9 @@ function typeofTest(){
 	console.log(typeof [1,3,4]);
 	console.log(typeof true);
 	console.log(typeof {name:'John', age:18});
+
+	//NaN的类型是数字，但是这个真实的意思是 不是一个数字
+	console.log(typeof NaN);
 }
 //特殊类型,当一个变量没有赋值的时候，值和类型为undefined
 function typeofTest1(){
@@ -169,4 +172,42 @@ function typeofTest1(){
 	//null和undefined 值相等，类型不同
 	console.log(null == undefined);
 	console.log(null === undefined);
+}
+
+//根据构造函数来判断数据类型
+//constructor 返回构造函数
+function getType(inputArray){
+	console.log(inputArray.constructor);
+	console.log(inputArray.constructor.toString());
+	console.log(inputArray.constructor.toString().indexOf("Array"));
+
+	console.log(new Date().constructor)
+	console.log(new Date().constructor.toString())
+}
+//类型转换
+function typeCast(){
+	//转换成字符串有两种方式，toString() 和构造函数
+	var a = 10;
+	console.log(a.toString());
+	console.log(String(a));
+
+	var b = "3.14";
+	console.log(Number(b));
+	//c 不能转换成一个数字，这个时候的输出值是NaN，表示不是一个数字，但是类型依旧是数字类型
+	var c = "99 88";
+	console.log(Number(c));
+
+	console.log(Boolean(c));
+
+	//+ 一元运算符  把字符串转换成数字
+	var x = "5"
+	var y = +x;
+	console.log(y)
+	console.log(typeof y);
+
+	var a = "John";
+	var b = +a;
+	console.log(b);
+	console.log(typeof b);
+
 }
